8e54ce0c2a182c36b8a1538bf1895c135100c686
Molk [Yeah, It's Me] <97996807+MolkOficial@users.noreply.github.com>
2025-04-16 19:19:36 -0400

    Initial commit

---
+++ project.lua
@@ -0,0 +1,26 @@
+return {
+	DEBUG_MODE = false,
+
+	title = "NX Interface To FNF",
+	file = "FNF-NX",
+	icon = "art/icon.png",
+	version = "0.0.1a?",
+	package = "fanengine.molk.fnfnx",
+	width = 1280,
+	height = 720,
+	FPS = 60,
+	company = "Molk",
+
+	flags = {
+		checkForUpdates = false,
+
+		loxelInitialAutoPause = true,
+		loxelInitialParallelUpdate = true,
+		loxelInitialAsyncInput = true,
+
+		loxelForceRenderCameraComplex = true,
+		loxelDisableRenderCameraComplex = false,
+		loxelDisableScissorOnRenderCameraSimple = false,
+		loxelDefaultClipCamera = true
+	}
+}
